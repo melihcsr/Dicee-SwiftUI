4c1b543e5ab44426fd8a4e4a9395a75605287eb9
Melih Cesur <mlhcsr@yahoo.com>
2023-09-18 15:47:56 +0300

    project added

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Melih Cesur on 18.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                Image("diceeLogo")
+                Spacer()
+                
+                HStack{
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                       
+                }
+                .padding(.horizontal)
+                Spacer()
+                
+                Button(action: {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }){
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(.red)
+                .padding(.bottom)
+             
+            }
+            
+          
+               
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n : Int
+    
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1,contentMode: .fit)
+            .padding()
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+}
---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Melih Cesur on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
